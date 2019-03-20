9af843a63e2ba2cd70406fd81f51028cbd81e86f
okothkongo <okotho@students.ku.ac.ke>
2019-03-20 08:08:23 +0300

    validate ownersignup-web

--- web/src/components/Ownersignup.js
+++ web/src/components/Ownersignup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
- import "./Ownersignup.css";
+
 class Ownersignup extends  Component {
 
     constructor(props) {
@@ -9,19 +9,58 @@ class Ownersignup extends  Component {
             lastName:'',
             email:'',
             password:'',
-            confirmPassword:''
+            confirmPassword:'',
+            firstNameError: "",
+            lastNameError: "",
+            emailError: "",
+            passwordError: "",
+            confirmPasswordError:'',
         };
         this.handleChange =this.handleChange.bind(this);
         this.handleSubmit= this.handleSubmit.bind(this);
     }
+
     handleChange (event) {
         this.setState({
             [event.target.name]: event.target.value
         });
     }
-     handleSubmit(event) {
-         event.preventDefault();
+    validate =()=>{
+        let firstNameError = '';
+        let lastNameError = '';
+        let emailError = '';
+        let passwordError = '';
+        let confirmPasswordError =''
+        const emailRegex= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/;
+        if ((this.state.firstName).trim() ===''){
+            firstNameError ='First Name Cannot be Empty'
+        }
 
+        if ((this.state.lastName).trim() ===''){
+            lastNameError ='Last Name Cannot be Empty'
+        }
+        if(!this.state.email.match(emailRegex)){
+            emailError = 'Invalid email'
+        }
+        if(!this.state.password.match(passwordRegex)){
+            passwordError = 'Enter stronger password'
+        }
+        if (!(this.state.confirmPassword === this.state.password)){
+            confirmPasswordError = 'password did not match'
+        }
+        if(lastNameError || firstNameError || emailError || passwordError || confirmPasswordError){
+            this.setState({lastNameError ,firstNameError, emailError, passwordError, confirmPasswordError});
+            return false;
+        }
+
+        return true;
+
+    }
+      handleSubmit(event) {
+         event.preventDefault();
+        const isValid = this.validate()
+        if(isValid){
          const ownerData = {
              firstName : this.state.firstName,
              lastName : this.state.lastName,
@@ -29,7 +68,6 @@ class Ownersignup extends  Component {
              password: this.state.password,
              confirmPassword : this.state.confirmPassword
          }
-         console.group(ownerData)
          fetch("https://mamamboga-api.herokuapp.com/api/v0/Owner/admin_auth", {
             method: "POST",
                body: JSON.stringify(ownerData),
@@ -37,26 +75,30 @@ class Ownersignup extends  Component {
                  "Content-Type": "application/json"
                },
                credentials: "same-origin"
-             }).then(function(response){
-              console.log(response);
-          })
+             })
      }
-    
+     }
+
 render () {
     return (
         <div className="sign_up_form">
             <h1>Sign Up Form</h1>
             <form onSubmit = {this.handleSubmit}>
             <input type ='text' placeholder='First Name' name ='firstName' onChange={this.handleChange}></input>
-            <br/>            
+            <br/>
+            <div>{this.state.firstNameError}</div>
             <input type ='text' placeholder='Last Name' name ='lastName' onChange={this.handleChange}></input>
             <br/>
+            <div>{this.state.lastNameError}</div>
             <input type ='email' placeholder='Email' name ='email' onChange={this.handleChange}></input>
             <br/>
+            <div>{this.state.emailError}</div>
             <input type ='password' placeholder='Password' name ='password' onChange={this.handleChange}></input>
             <br/>
+            <div>{this.state.passwordError}</div>
             <input type ='password' placeholder='Confirm Password' name ='confirmPassword' onChange={this.handleChange}></input>
             <br/>
+            <div>{this.state.confirmPasswordError}</div>
             <button type= "submit">CREATE ACCOUNT</button>
             </form>
          </div>
